efcef51f4782926a9e7f2f670ad91a9a47b57938
tfederico <federicotavella.7@gmail.com>
2016-04-26 16:01:04 +0200

    Modifiche librerie

--- app/src/main/java/com/leaf/clips/model/beacon/BeaconManagerAdapter.java
+++ app/src/main/java/com/leaf/clips/model/beacon/BeaconManagerAdapter.java
@@ -10,7 +10,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.support.annotation.Nullable;
+import javax.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
 
 import org.altbeacon.beacon.Beacon;
