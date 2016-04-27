ea70f67d151cf05022d95efb38342072f29bcf85
oeconti <octatux@gmail.com>
2016-04-27 17:09:33 +0200

    Codified test of DetailedInformation

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/navigationinformation/DetailedInformationTest.java
@@ -0,0 +1,27 @@
+package com.leaf.clips.model.navigator.graph.navigationinformation;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public class DetailedInformationTest {
+
+    private DetailedInformation detailedInformation;
+
+    @Before
+    public void init() throws Exception {
+        this.detailedInformation = new DetailedInformation("DetailedInformation");
+    }
+
+    @Test
+    public void testGetDetailedInformation() throws Exception {
+        Assert.assertEquals("DetailedInformation", detailedInformation.getDetailedInformation());
+    }
+}
