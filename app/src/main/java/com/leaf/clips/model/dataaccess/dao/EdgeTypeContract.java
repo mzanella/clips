6e1728059fcfd05ca0c7ff829828985d5d9f2ff6
mzanella <zanna0150@gmail.com>
2016-04-28 16:05:01 +0200

    Creazione classe EdgeTypeContract

---
+++ app/src/main/java/com/leaf/clips/model/dataaccess/dao/EdgeTypeContract.java
@@ -0,0 +1,30 @@
+package com.leaf.clips.model.dataaccess.dao;
+/**
+ * @author Marco Zanella
+ * @version 0.01
+ * @since 0.01
+ *
+ *
+ */
+
+/**
+ *Classe che contiene le informazioni corrette della tabella EdgeType del database locale
+ */
+public class EdgeTypeContract {
+
+    /**
+     * Valore della colonna id. Valore di default "id"
+     */
+    public final static String COLUMN_ID = "id";
+
+    /**
+     * Valore della colonna typeName. Valore di default "typeName"
+     */
+    public final static String COLUMN_TYPENAME = "typeName";
+
+    /**
+     * Nome della tabella. Valore di default "EdgeType"
+     */
+    public final static String TABLE_NAME = "EdgeType";
+
+}
