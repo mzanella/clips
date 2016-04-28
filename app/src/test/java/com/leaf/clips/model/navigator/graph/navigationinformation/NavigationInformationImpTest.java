0c265fa394e6c1b8a4aef6a59cdf2cf847ad4fcb
oeconti <octatux@gmail.com>
2016-04-28 15:52:53 +0200

    Added NavigationInformationImpTest class

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/navigationinformation/NavigationInformationImpTest.java
@@ -0,0 +1,28 @@
+package com.leaf.clips.model.navigator.graph.navigationinformation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public class NavigationInformationImpTest {
+
+    @Test
+    public void testGetBasicInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDetailedInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPhotoInformation() throws Exception {
+
+    }
+}
