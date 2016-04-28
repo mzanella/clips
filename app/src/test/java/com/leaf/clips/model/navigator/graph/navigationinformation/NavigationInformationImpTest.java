151130fa632d5cada5573872853a1ebd1df5e214
oeconti <octatux@gmail.com>
2016-04-28 16:04:31 +0200

    Codified all test in NavigationInformationImp

--- app/src/test/java/com/leaf/clips/model/navigator/graph/navigationinformation/NavigationInformationImpTest.java
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/navigationinformation/NavigationInformationImpTest.java
@@ -1,6 +1,13 @@
 package com.leaf.clips.model.navigator.graph.navigationinformation;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
@@ -11,18 +18,45 @@ import static org.junit.Assert.*;
  */
 public class NavigationInformationImpTest {
 
+    private NavigationInformationImp navigationInformationImp;
+    private PhotoInformation photoInformation;
+
+    @Before
+    public void init() {
+        //Create a basic info
+        BasicInformation basicInformation = new BasicInformation("Basic Instruction");
+
+        //Create a detailed info
+        DetailedInformation detailedInformation = new DetailedInformation("Detailed Instruction");
+
+        //Create a Collection of PhotoRef
+        Collection<PhotoRef> photoRefs = new ArrayList<>();
+        try {
+            photoRefs.add(new PhotoRef(new URI("www.google.com")));
+            photoRefs.add(new PhotoRef(new URI("www.apple.com")));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        //Create a PhotoInformation
+        this.photoInformation = new PhotoInformation(photoRefs);
+
+        this.navigationInformationImp = new NavigationInformationImp(basicInformation, detailedInformation, photoInformation);
+    }
+
     @Test
     public void testGetBasicInformation() throws Exception {
-
+        Assert.assertEquals("Basic Instruction", navigationInformationImp.getBasicInformation());
     }
 
     @Test
     public void testGetDetailedInformation() throws Exception {
-
+        Assert.assertEquals("Detailed Instruction", navigationInformationImp.getDetailedInformation());
     }
 
     @Test
     public void testGetPhotoInformation() throws Exception {
-
+        Assert.assertNotNull(navigationInformationImp.getPhotoInformation());
+        Assert.assertEquals(photoInformation, navigationInformationImp.getPhotoInformation());
     }
 }
