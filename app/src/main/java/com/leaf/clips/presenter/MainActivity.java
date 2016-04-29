a24973ea42d276b0f472a006488b031e300fb278
Andrea Tombolato <tombolato.andrea@gmail.com>
2016-04-29 18:33:42 +0200

    added SplashTheme

--- app/src/main/java/com/leaf/clips/presenter/MainActivity.java
+++ app/src/main/java/com/leaf/clips/presenter/MainActivity.java
@@ -1,15 +1,23 @@
 package com.leaf.clips.presenter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.leaf.clips.R;
 
+/**
+ *  MainActivity ha l'unico compito di reindirizzare l'utente verso HomeActivity e fornire una
+ *  schermata di caricamento in caso di computazioni pesanti all'avvio.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
