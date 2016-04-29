6f1cf8a1aa0c571de969065095143ca419191901
oeconti <octatux@gmail.com>
2016-04-29 16:21:47 +0200

    Added test class VertexImp

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/vertex/VertexImpTest.java
@@ -0,0 +1,25 @@
+package com.leaf.clips.model.navigator.graph.vertex;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public class VertexImpTest {
+
+    private VertexImp vertexImp;
+
+    @Before
+    public void init() {
+        this.vertexImp = new VertexImp(15);
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        Assert.assertEquals(15, vertexImp.getId());
+    }
+}
