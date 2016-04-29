c289d2e66cd5b3d95b3b90ce52bf56a5c7e17b2e
oeconti <octatux@gmail.com>
2016-04-29 16:08:31 +0200

    Added Vertex interface

---
+++ app/src/main/java/com/leaf/clips/model/navigator/graph/Vertex/Vertex.java
@@ -0,0 +1,20 @@
+package com.leaf.clips.model.navigator.graph.Vertex;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+/**
+ *Interfaccia che serve per descrivere le funzionalità di un vertice di un grafo
+ */
+public interface Vertex {
+
+    /**
+     * Metodo che ritorna l'identificativo numerico associato al vertice
+     * @return  int
+     */
+    int getId();
+
+}
+
