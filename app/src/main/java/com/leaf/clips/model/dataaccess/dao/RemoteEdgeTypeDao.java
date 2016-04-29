a458bf083c034754096fe5cc4c9b07f3d5377e5a
mzanella <zanna0150@gmail.com>
2016-04-29 20:42:50 +0200

    classe e test RemoteEdgeTypeDao

---
+++ app/src/main/java/com/leaf/clips/model/dataaccess/dao/RemoteEdgeTypeDao.java
@@ -0,0 +1,61 @@
+package com.leaf.clips.model.dataaccess.dao;
+/**
+ * @author Marco Zanella
+ * @version 0.01
+ * @since 0.01
+ *
+ *
+ */
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import junit.framework.Assert;
+
+import java.lang.reflect.Type;
+
+/**
+ *Classe di utility per la conversione da JSON a EdgeTypeTable
+ */
+public class RemoteEdgeTypeDao {
+
+    // TODO: 29/04/16 chiedere modifica classe
+    private static final Gson gson = new Gson();
+
+    /**
+     * Costruttore di default per la classe RemoteEdgeTypeDao
+     */
+    public RemoteEdgeTypeDao(){}
+
+    /**
+     * Metodo utilizzato per la conversione di un oggetto JsonObject in un oggetto EdgeTypeTable, che viene ritornato
+     * @param object Oggetto JSON che rappresenta un oggetto di tipo EdgeTypeTable
+     * @return  EdgeTypeTable
+     */
+    public EdgeTypeTable fromJSONToTable(JsonObject object) {
+        // TODO: 29/04/16 chiedere perchè no nomi uguali
+        GsonBuilder gsonBldr = new GsonBuilder();
+        JsonDeserializer<EdgeTypeTable> deserializer = new JsonDeserializer<EdgeTypeTable>() {
+            @Override
+            public EdgeTypeTable deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+                JsonObject jObject = json.getAsJsonObject();
+                int deserializatedId = jObject.get("id").getAsInt();
+                String deserializedTypeName = jObject.get("description").getAsString();
+                Assert.assertNotNull(deserializedTypeName);
+                return new EdgeTypeTable(deserializatedId, deserializedTypeName);
+            }
+        };
+        gsonBldr.registerTypeAdapter(EdgeTypeTable.class, deserializer);
+        EdgeTypeTable edgeTypeTable;
+        edgeTypeTable = gsonBldr.create().fromJson(object, EdgeTypeTable.class);
+        return edgeTypeTable;
+    }
+
+}
---
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemoteEdgeTypeDaoTest.java
@@ -0,0 +1,41 @@
+package com.leaf.clips.model.dataaccess.dao;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zanna on 29/04/16.
+ */
+public class RemoteEdgeTypeDaoTest {
+
+    private JsonObject js;
+    private EdgeTypeTable edgeTypeTable;
+    private RemoteEdgeTypeDao remoteEdgeTypeDao;
+    private final String jsString = "{"
+            +"\"id\" : 1,"
+            +"\"description\" : \"description\""
+        +"}";
+    private JsonParser parser;
+
+    @Before
+    public void init() {
+        parser = new JsonParser();
+        js = parser.parse(jsString).getAsJsonObject();
+        remoteEdgeTypeDao = new RemoteEdgeTypeDao();
+        edgeTypeTable = remoteEdgeTypeDao.fromJSONToTable(js);
+    }
+
+    @Test
+    public void testFromJSONToTable() throws Exception {
+        edgeTypeTable = remoteEdgeTypeDao.fromJSONToTable(js);
+        Assert.assertEquals(1, edgeTypeTable.getId());
+        Assert.assertEquals("description", edgeTypeTable.getTypeName());
+    }
+}
