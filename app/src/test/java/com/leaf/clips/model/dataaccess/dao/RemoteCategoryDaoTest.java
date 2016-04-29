a7d658e90798603a9974c57ae27e450887ec586a
mzanella <zanna0150@gmail.com>
2016-04-29 20:51:58 +0200

    classe e test RemoteCategoryDao

---
+++ app/src/test/java/com/leaf/clips/model/dataaccess/dao/RemoteCategoryDaoTest.java
@@ -0,0 +1,41 @@
+package com.leaf.clips.model.dataaccess.dao;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zanna on 29/04/16.
+ */
+public class RemoteCategoryDaoTest {
+
+    private JsonObject js;
+    private CategoryTable categoryTable;
+    private RemoteCategoryDao remoteCategoryDao;
+    private final String jsString = "{"
+            +"\"id\" : 1,"
+            +"\"description\" : \"description\""
+            +"}";
+    private JsonParser parser;
+
+    @Before
+    public void init() {
+        parser = new JsonParser();
+        js = parser.parse(jsString).getAsJsonObject();
+        remoteCategoryDao = new RemoteCategoryDao();
+        categoryTable = remoteCategoryDao.fromJSONToTable(js);
+    }
+
+    @Test
+    public void testFromJSONToTable() throws Exception {
+        categoryTable = remoteCategoryDao.fromJSONToTable(js);
+        Assert.assertEquals(1, categoryTable.getId());
+        Assert.assertEquals("description", categoryTable.getDescription());
+    }
+}
