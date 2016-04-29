4c53645871b446156026b89acc6cb0786418a223
oeconti <octatux@gmail.com>
2016-04-29 16:28:43 +0200

    Added PointOfInterestInformation class

---
+++ app/src/main/java/com/leaf/clips/model/navigator/graph/area/PointOfInterestInformation.java
@@ -0,0 +1,62 @@
+package com.leaf.clips.model.navigator.graph.area;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+/**
+ *Classe che rappresenta le informazioni associate ad un POI
+ */
+public class PointOfInterestInformation {
+
+    // TODO: 4/29/16 to codify 
+    
+    /**
+     * Categoria di appartenenza del POI
+     */
+    private final String  category = null;
+
+    /**
+     * Descrizione del POI
+     */
+    private final String  description = null;
+
+    /**
+     * Nome del POI
+     */
+    private final String  name = null;
+
+    /**
+     * Costruttore della classe PointOfInterestInformation
+     * @param name Nome del POI
+     * @param description Descrizione del POI
+     * @param category Categoria di appartenenza del POI
+     */
+    public PointOfInterestInformation(String name, String description, String category){}
+
+    /**
+     * Metodo che ritorna la categoria di appartenenza del PointOfInterest a cui l'oggetto è associato
+     * @return  String
+     */
+    public String getCategory(){
+        return null;
+    }
+
+    /**
+     * Metodo che ritorna la descrizione del PointOfInterest a cui l'oggetto è associato
+     * @return  String
+     */
+    public String getDescription() {
+        return null;
+    }
+
+    /**
+     * Metodo che ritorna il nome del PointOfInterest a cui l'oggetto è associato
+     * @return  String
+     */
+    public String getName() {
+        return null;
+    }
+
+}
