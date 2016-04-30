e1f7bd5d4c32a8f60a42ec65b491be4ce3533c64
oeconti <octatux@gmail.com>
2016-04-30 17:42:23 +0200

    Added Edge Interface

--- app/src/main/java/com/leaf/clips/model/navigator/graph/edge/Edge.java
+++ app/src/main/java/com/leaf/clips/model/navigator/graph/edge/Edge.java
@@ -5,5 +5,36 @@ package com.leaf.clips.model.navigator.graph.edge;
  * @version 0.01
  * @since 0.00
  */
+
+import com.leaf.clips.model.navigator.graph.vertex.Vertex;
+
+/**
+ *Interfaccia che serve per descrivere le funzionalità di un arco di un grafo
+ */
 public interface Edge {
+
+    /**
+     * Metodo che restituisce il Vertex di arrivo dell'arco
+     * @return  Vertex
+     */
+    Vertex getEndPoint();
+
+    /**
+     * Metodo che ritorna l'identificativo numerico dell'arco
+     * @return  int
+     */
+    int getId();
+
+    /**
+     * Metodo che restituisce il Vertex di partenza dell'arco
+     * @return  Vertex
+     */
+    Vertex getStarterPoint();
+
+    /**
+     * Metodo che ritorna il peso dell'arco
+     * @return  int
+     */
+    int getWeight();
+
 }
