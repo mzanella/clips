0044e133a3c5fff9794c8fd13e83d7eac0980ba6
mzanella <zanna0150@gmail.com>
2016-04-30 13:03:37 +0200

    Classe e test RemoteBuildingDao

--- app/src/main/java/com/leaf/clips/model/dataaccess/dao/RemoteBuildingDao.java
+++ app/src/main/java/com/leaf/clips/model/dataaccess/dao/RemoteBuildingDao.java
@@ -5,5 +5,61 @@ package com.leaf.clips.model.dataaccess.dao;
  * @version 0.01
  * @since 0.01
  */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+/**
+ *Classe di utility per la conversione da JSON a BuildingTable
+ */
 public class RemoteBuildingDao {
+
+    // TODO: 29/04/16 chiedere modifica alla classe
+    private static final Gson gson = new Gson();
+
+    /**
+     * Costruttore di default per la classe RemoteBuildingDao
+     */
+    public RemoteBuildingDao(){}
+
+    /**
+     * Metodo utilizzato per la conversione di un oggetto JsonObject in un oggetto BuildingTable, che viene ritornato
+     * @param object Oggetto JSON che rappresenta un oggetto di tipo BuildingTable
+     * @return  BuildingTable
+     */
+    public BuildingTable fromJSONToTable(JsonObject object){
+        // TODO: 30/04/16 chiedere perchè no nomi uguali
+        GsonBuilder gsonBldr = new GsonBuilder();
+        JsonDeserializer<BuildingTable> deserializer = new JsonDeserializer<BuildingTable>() {
+            @Override
+            public BuildingTable deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                    throws JsonParseException {
+                JsonObject jObject = json.getAsJsonObject();
+                int deserializatedId = jObject.get("id").getAsInt();
+                String deserializatedUuid = jObject.get("uuid").getAsString();
+                int deserializatedMajor = jObject.get("major").getAsInt();
+                String deserializatedName = jObject.get("name").getAsString();
+                String deserializedDescription = jObject.get("description").getAsString();
+                String deserializedOpeningHours = jObject.get("openingHours").getAsString();
+                String deserializedAddress = jObject.get("address").getAsString();
+                int deserializedMapVersion = jObject.get("mapVersion").getAsInt();
+                String deserializedMapSize = jObject.get("dimension").getAsString();
+
+                return new BuildingTable(deserializatedId, deserializatedUuid, deserializatedMajor,
+                        deserializatedName, deserializedDescription, deserializedOpeningHours,
+                        deserializedAddress, deserializedMapVersion, deserializedMapSize);
+            }
+        };
+        gsonBldr.registerTypeAdapter(BuildingTable.class, deserializer);
+        BuildingTable buildingTable;
+        buildingTable = gsonBldr.create().fromJson(object, BuildingTable.class);
+        return buildingTable;
+    }
+
 }
