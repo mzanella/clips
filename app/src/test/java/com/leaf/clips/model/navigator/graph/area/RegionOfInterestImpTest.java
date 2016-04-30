35a0a3816db7250d0a777c83b1314f24ab4e2a03
oeconti <octatux@gmail.com>
2016-04-30 14:39:51 +0200

    Added RegionOfInterestImpTest

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/area/RegionOfInterestImpTest.java
@@ -0,0 +1,51 @@
+package com.leaf.clips.model.navigator.graph.area;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+
+// TODO: 4/30/16 codify
+
+public class RegionOfInterestImpTest {
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllNearbyPOIs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFloor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMajor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMinor() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUUID() throws Exception {
+
+    }
+
+    @Test
+    public void testSetNearbyPOIs() throws Exception {
+
+    }
+}
