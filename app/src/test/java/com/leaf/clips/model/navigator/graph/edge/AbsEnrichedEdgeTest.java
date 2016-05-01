2666840c6d3c2d268847812c20f73888440f7899
oeconti <octatux@gmail.com>
2016-05-01 17:29:27 +0200

    Added AbsEnrichedEdge test class

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/edge/AbsEnrichedEdgeTest.java
@@ -0,0 +1,63 @@
+package com.leaf.clips.model.navigator.graph.edge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+public class AbsEnrichedEdgeTest {
+
+    @Test
+    public void testGetBasicInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCoordinate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDetailedInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDistance() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEndPoint() throws Exception {
+
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNavigationInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPhotoInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStarterPoint() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWeight() throws Exception {
+
+    }
+}
