ddcf81aa4afa4c49e17d9bb68bc18c68fa85c2d4
mzanella <zanna0150@gmail.com>
2016-05-01 16:04:45 +0200

    modifica classe PointOfInterestContract

---
+++ app/src/androidTest/java/com/leaf/clips/model/dataaccess/dao/SQLitePointOfInterestDaoTest.java
@@ -0,0 +1,43 @@
+package com.leaf.clips.model.dataaccess.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Marco Zanella
+ * @version 0.01
+ * @since 0.01
+ */
+public class SQLitePointOfInterestDaoTest {
+
+    @Test
+    public void testCursorToType() throws Exception {
+
+    }
+
+    @Test
+    public void testDeletePointOfInterest() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAllPointsWithMajor() throws Exception {
+
+    }
+
+    @Test
+    public void testFindPointOfInterest() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertPointOfInterest() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdatePointOfInterest() throws Exception {
+
+    }
+}
