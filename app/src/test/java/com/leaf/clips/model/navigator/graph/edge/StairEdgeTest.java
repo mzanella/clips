fdd857fd62974cfb7b7cac645afe4723054b5437
oeconti <octatux@gmail.com>
2016-05-01 17:23:40 +0200

    Added StairEdgeTest class

---
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/edge/StairEdgeTest.java
@@ -0,0 +1,31 @@
+package com.leaf.clips.model.navigator.graph.edge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+
+// TODO: 5/1/16 Codify 
+
+public class StairEdgeTest {
+
+    @Test
+    public void testGetBasicInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDetailedInformation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWeight() throws Exception {
+
+    }
+}
