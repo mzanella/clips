165a58484a9d160d3d7da0e019843e5f80c38365
Andrea Tombolato <tombolato.andrea@gmail.com>
2016-05-02 21:09:38 +0200

    first added

---
+++ app/src/main/java/com/leaf/clips/view/DeveloperUnlockerViewImp.java
@@ -0,0 +1,13 @@
+package com.leaf.clips.view;
+
+/**
+ * @author Andrea Tombolato
+ * @version 0.01
+ * @since 0.00
+ */
+public class DeveloperUnlockerViewImp implements DeveloperUnlockerView {
+    @Override
+    public void showWrongCode() {
+
+    }
+}
