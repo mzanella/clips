11d5345bf6e9de1ed0ab51a417dc9ad7a47f827b
tfederico <federicotavella.7@gmail.com>
2016-05-02 18:04:48 +0200

    Aggiornato NavigationListener

--- app/src/main/java/com/leaf/clips/model/NavigationListener.java
+++ app/src/main/java/com/leaf/clips/model/NavigationListener.java
@@ -9,4 +9,9 @@ package com.leaf.clips.model;
  * Class Description
  */
 public interface NavigationListener {
+    // TODO: 02/05/2016 da aggiungere su Tracy
+
+    void informationUpdate(ProcessedInformation info);
+
+    void pathError();
 }
