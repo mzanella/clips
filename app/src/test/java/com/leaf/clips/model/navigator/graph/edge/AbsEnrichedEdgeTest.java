a93b454762cec1200c8fbdbd0c9936de59884488
black2279 <black.andrighetto94@gmail.com>
2016-05-03 01:33:01 +0200

    Aggiunti test per i metodi non astratti

--- app/src/test/java/com/leaf/clips/model/navigator/graph/edge/AbsEnrichedEdgeTest.java
+++ app/src/test/java/com/leaf/clips/model/navigator/graph/edge/AbsEnrichedEdgeTest.java
@@ -1,63 +1,153 @@
 package com.leaf.clips.model.navigator.graph.edge;
 
+import com.leaf.clips.model.beacon.MyBeacon;
+import com.leaf.clips.model.beacon.MyBeaconImp;
+import com.leaf.clips.model.navigator.graph.area.RegionOfInterest;
+import com.leaf.clips.model.navigator.graph.area.RegionOfInterestImp;
+import com.leaf.clips.model.navigator.graph.navigationinformation.BasicInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.DetailedInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.NavigationInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.NavigationInformationImp;
+import com.leaf.clips.model.navigator.graph.navigationinformation.PhotoInformation;
+import com.leaf.clips.model.navigator.graph.navigationinformation.PhotoRef;
+
+import org.altbeacon.beacon.AltBeacon;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
- * @author Oscar Elia Conti
+ * @author Cristian Andrighetto
  * @version 0.01
  * @since 0.00
  */
 public class AbsEnrichedEdgeTest {
 
-    @Test
-    public void testGetBasicInformation() throws Exception {
+    private DefaultEdge defaultEdge;
 
+    int startId = 46;
+    String startUuid = "DF7E1C79-43E9-44FF-886F-1D1F7DA6997A".toLowerCase();
+    int startMajor = 2;
+    int startMinor = 4;
+
+    int endId = 99;
+    String endUuid = "DF7E1C79-43E9-44FF-887F-1D1F7DA6997A".toLowerCase();
+    int endMajor = 6;
+    int endMinor = 10;
+
+    double distance = 20;
+    int edgeID = 1;
+    int coordinate = 12;
+    NavigationInformation navInfo;
+
+    MyBeacon myStartBeacon;
+    MyBeacon myEndBeacon;
+
+    RegionOfInterest startROI;
+    RegionOfInterest endROI;
+
+    @Before
+    public void init() {
+
+        //Construct the Beacons for tests
+        List<Long> data = new LinkedList<>();
+        data.add((long) 88);
+        myStartBeacon= new MyBeaconImp(new AltBeacon.Builder().setId1(startUuid)
+                .setId2(((Integer)startMajor).toString()).setId3(((Integer)startMinor).toString()).setRssi(-55).setTxPower(-55).setBluetoothAddress("prova").setDataFields(data).setBeaconTypeCode(1).build());
+        myEndBeacon= new MyBeaconImp(new AltBeacon.Builder().setId1(endUuid)
+                .setId2(((Integer)endMajor).toString()).setId3(((Integer)endMinor).toString()).setRssi(-55).setTxPower(-55).setBluetoothAddress("prova").setDataFields(data).setBeaconTypeCode(1).build());
+
+        this.startROI = new RegionOfInterestImp(this.startId, this.startUuid, this.startMajor, this.startMinor);
+        this.endROI = new RegionOfInterestImp(this.endId, this.endUuid, this.endMajor, this.endMinor);
+
+        //Create a basic info
+        BasicInformation basicInformation = new BasicInformation("Basic Instruction");
+
+        //Create a detailed info
+        DetailedInformation detailedInformation = new DetailedInformation("Detailed Instruction");
+
+        //Create a Collection of PhotoRef
+        Collection<PhotoRef> photoRefs = new ArrayList<>();
+        try {
+            photoRefs.add(new PhotoRef(new URI("www.google.com")));
+            photoRefs.add(new PhotoRef(new URI("www.apple.com")));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        //Create a PhotoInformation
+        PhotoInformation photoInformation = new PhotoInformation(photoRefs);
+
+        this.navInfo = new NavigationInformationImp(basicInformation, detailedInformation, photoInformation);
+
+        defaultEdge = new DefaultEdge(startROI, endROI, distance, coordinate, edgeID, navInfo);
     }
-
+    /*
+     * TU14
+     */
     @Test
     public void testGetCoordinate() throws Exception {
-
-    }
-
-    @Test
-    public void testGetDetailedInformation() throws Exception {
-
+         assertEquals(coordinate,defaultEdge.getCoordinate());
     }
 
     @Test
     public void testGetDistance() throws Exception {
-
+        assertEquals(distance,defaultEdge.getDistance(),0.01);
     }
 
+    /*
+     * TU14
+     */
     @Test
     public void testGetEndPoint() throws Exception {
-
+        assertEquals(endROI,defaultEdge.getEndPoint());
     }
 
+    /*
+     * TU14
+     */
     @Test
     public void testGetId() throws Exception {
-
+        assertEquals(edgeID,defaultEdge.getId());
     }
 
+    /*
+     * TU16
+     */
     @Test
     public void testGetNavigationInformation() throws Exception {
-
+        assertEquals(navInfo,defaultEdge.getNavigationInformation());
     }
 
+    /*
+     * TU14
+     */
     @Test
     public void testGetPhotoInformation() throws Exception {
+        URI uris[] = {new URI("www.google.com"),new URI("www.apple.com")};
+        Collection<PhotoRef> photoRefCollection = defaultEdge.getPhotoInformation().getPhotoInformation();
+        int i=0;
+        for(PhotoRef p:photoRefCollection){
+            assertEquals(p.getPhotoUri(),uris[i]);
+            i++;
+        }
 
     }
 
+    /*
+     * TU14
+     */
     @Test
     public void testGetStarterPoint() throws Exception {
-
+        assertEquals(startROI,defaultEdge.getStarterPoint());
     }
 
-    @Test
-    public void testGetWeight() throws Exception {
-
-    }
 }
