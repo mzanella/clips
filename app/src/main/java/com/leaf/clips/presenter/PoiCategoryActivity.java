5ae6666ae1aeab6febc5b8ae3d67b6eb74fae381
oeconti <octatux@gmail.com>
2016-05-03 14:52:45 +0200

    Added PoiCategoryActivity

---
+++ app/src/main/java/com/leaf/clips/presenter/PoiCategoryActivity.java
@@ -0,0 +1,15 @@
+package com.leaf.clips.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.leaf.clips.R;
+
+public class PoiCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poi_category);
+    }
+}
