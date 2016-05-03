e308a3136d3912145d5f9d3cffdd4d354d537394
oeconti <octatux@gmail.com>
2016-05-03 15:25:45 +0200

    Imported NearbyPoiView

--- app/src/main/java/com/leaf/clips/presenter/NearbyPoiActivity.java
+++ app/src/main/java/com/leaf/clips/presenter/NearbyPoiActivity.java
@@ -4,8 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.leaf.clips.R;
+import com.leaf.clips.view.NearbyPoiView;
 
 public class NearbyPoiActivity extends AppCompatActivity {
+
+    /**
+     * Insieme di POI rilevati nelle circostanze
+     */
+    //private Collection<PointOfInterest> pois;
+
+    /**
+     * View associata a tale Activity
+     */
+    private NearbyPoiView view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
