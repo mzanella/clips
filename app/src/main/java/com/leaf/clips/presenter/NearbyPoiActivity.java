5ccc57d3793105f453d6037a0ccc9cadb4660350
oeconti <octatux@gmail.com>
2016-05-03 15:20:08 +0200

    Added NearbyPoiActivity

---
+++ app/src/main/java/com/leaf/clips/presenter/NearbyPoiActivity.java
@@ -0,0 +1,13 @@
+package com.leaf.clips.presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NearbyPoiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_poi);
+    }
+}
