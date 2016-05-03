3466273fd984e3e056fc8c069bca12f8a88f5ffd
oeconti <octatux@gmail.com>
2016-05-03 14:32:27 +0200

    Added all methods HomeActivity Todo implementations

--- app/src/main/java/com/leaf/clips/presenter/HomeActivity.java
+++ app/src/main/java/com/leaf/clips/presenter/HomeActivity.java
@@ -10,16 +10,25 @@ import com.leaf.clips.view.HomeView;
 import com.leaf.clips.view.HomeViewImp;
 
 public class HomeActivity extends AppCompatActivity {
-    HomeView homeView;
     /**
      *  TODO
-        InformationManager informationManager;
+        Enable Suggestion
      */
+
+    /**
+     * Riferimento utilizzato per accedere alle informazioni trattate dal model
+     */
+    //private InformationManager informationManager;
+
+    /**
+     * View associata a tale Activity
+     */
+    private HomeView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        homeView = new HomeViewImp(this);
+        view = new HomeViewImp(this);
     }
 
     /**
@@ -36,4 +45,103 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
     }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di visualizzazione della modalità esplora
+     * @return  void
+     */
+    public void showExplorer(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di visualizzazione della guida
+     * @return  void
+     */
+    public void showHelp(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di visualizzazione della mappe salvate nel database locale
+     * @return  void
+     */
+    public void showLocalMaps(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di visualizzazione di tutti i POI appartenenti ad un certa categoria
+     * @param categoryName Nome della categoria di cui visualizzare l'insieme di POI appartenente
+     * @return  void
+     */
+    public void showPoisCategory(String categoryName){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di visualizzazione delle preferenze dell'utente
+     * @return  void
+     */
+    public void showPreferences(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che viene invocato a seguito della richiesta di inizio della navigazione
+     * @param poiPosition Identificativo del POI verso il quale si vuole effettuare una navigazione
+     * @return  void
+     */
+    public void startNavigation(int poiPosition){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che recupera l'indirizzo dell'edificio e lo passa alla View corrispondente
+     * @return  void
+     */
+    public void updateBuildingAddress(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che recupera la descrizione dell'edificio e lo passa alla View corrispondente
+     * @return  void
+     */
+    public void updateBuildingDescription(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che recupera il nome dell'indirizzo dell'edificio e lo passa alla View corrispondente
+     * @return  void
+     */
+    public void updateBuildingName(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che recupera l'orario di apertura dell'edificio e lo passa alla View corrispondente
+     * @return  void
+     */
+    public void updateBuildingOpeningHours(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che recupera la lista di categorie di POI nell'edificio e lo passa alla View corrispondente
+     * @return  void
+     */
+    public void updatePoiCategoryList(){
+        // TODO: 5/3/16  
+    }
+
+    /**
+     * Metodo che permette di attivare la lista dei possibili POI raggiungibili a partire da una stringa
+     * @return  void
+     */
+    public void enableSuggestions(){
+        // TODO: 5/3/16  
+    }
+
 }
