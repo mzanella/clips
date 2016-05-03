ec664ba28ca16efb5d77787ecb338a975a5b618a
oeconti <octatux@gmail.com>
2016-05-03 15:26:52 +0200

    Added methods on NearbyPoiView

---
+++ app/src/main/java/com/leaf/clips/view/NearbyPoiViewImp.java
@@ -0,0 +1,12 @@
+package com.leaf.clips.view;
+
+/**
+ * @author Oscar Elia Conti
+ * @version 0.01
+ * @since 0.00
+ */
+
+// TODO: 5/3/16 Codify 
+
+public class NearbyPoiViewImp implements NearbyPoiView{
+}
